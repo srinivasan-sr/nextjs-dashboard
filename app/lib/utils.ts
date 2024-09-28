4ee8f6e3b3613cbea467f0cb8c42bb2b89e1188d
srinivasan-sr <srinivasan_education@outlook.in>
2024-09-29 01:39:38 +0530

    Added Dashboard and Invoices pages; changes to display latest invoice and revenue charts

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,15 +1,15 @@
 import { Revenue } from './definitions';
 
 export const formatCurrency = (amount: number) => {
-  return (amount / 100).toLocaleString('en-US', {
+  return (amount / 100).toLocaleString('en-IN', {
     style: 'currency',
-    currency: 'USD',
+    currency: 'INR',
   });
 };
 
 export const formatDateToLocal = (
   dateStr: string,
-  locale: string = 'en-US',
+  locale: string = 'en-IN',
 ) => {
   const date = new Date(dateStr);
   const options: Intl.DateTimeFormatOptions = {
